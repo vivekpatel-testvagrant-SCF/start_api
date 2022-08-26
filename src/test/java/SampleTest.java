e391b2e6213b6c685fe79d4b2739d285668d4f2f
vivek1137 <vivekp@testvagrant.com>
2022-08-26 10:04:08 +0530

    Initial Project needed files

---
+++ src/test/java/SampleTest.java
@@ -0,0 +1,10 @@
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class SampleTest {
+
+    @Test
+    public void testIsValidating() {
+        Assert.assertEquals(1,1);
+    }
+}
