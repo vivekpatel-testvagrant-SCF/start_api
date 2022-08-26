486c1bcfd77190aa4358be728b4530b4e8578203
vivek1137 <vivekp@testvagrant.com>
2022-08-26 10:25:17 +0530

    Test Case of Rest Assured and Dependencies

--- src/test/java/SampleTest.java
+++ src/test/java/SampleTest.java
@@ -1,10 +1,20 @@
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
+import static io.restassured.RestAssured.given;
 public class SampleTest {
 
     @Test
-    public void testIsValidating() {
-        Assert.assertEquals(2,2);
+    public void shouldGetAllUsers() {
+        // Arrange
+        //Act
+        //Assert
+        given()
+                .when()
+                .get("https://gorest.co.in/public/v2/users/3")
+                .then()
+                .statusCode(200)
+                .log().body();
+
     }
 }
